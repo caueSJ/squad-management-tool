c3e2c507a19191d3fe53deec1ab4080465a64bfd
Caue SJ <caue.silvaa@gmail.com>
2020-09-24 22:58:31 -0300

    Initial commit + Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import VenturusLogo from '../../LogoVenturus.svg';
+import './Header.scss';
+
+const Header = () => {
+    return (
+        <header className="Header">
+            <a href="./" title="Home Page">
+                <img className="venturus-logo" src={VenturusLogo} alt="Venturus"></img>
+                <span className="product-name">Squad Management Tool</span>
+            </a>
+
+            <div>
+                <span className="user-name">John Doe</span>
+                <span className="fake-icon" title="John Doe">JD</span>
+            </div>
+        </header>
+    )
+};
+
+export default Header;
