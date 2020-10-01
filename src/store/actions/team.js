d33678306aa7dd72192c2eb5774cbf202a1a8eea
Caue SJ <caue.silvaa@gmail.com>
2020-10-01 01:42:31 -0300

    Update team reducer

---
+++ src/store/actions/team.js
@@ -0,0 +1,22 @@
+import * as actionTypes from './actionTypes';
+
+export const addTeam = team => {
+    return {
+        type: actionTypes.ADD_TEAM,
+        team
+    };
+};
+
+export const editTeam = team => {
+    return {
+        type: actionTypes.EDIT_TEAM,
+        team
+    };
+};
+
+export const deleteTeam = id => {
+    return {
+        type: actionTypes.DELETE_TEAM,
+        id
+    };
+};
