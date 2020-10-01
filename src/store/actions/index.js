667eb7e200c34c89a92c275a02df3a5c9c9a8529
Caue SJ <caue.silvaa@gmail.com>
2020-10-01 02:03:37 -0300

    Team reducer

---
+++ src/store/actions/index.js
@@ -0,0 +1,5 @@
+export {
+    addTeam,
+    deleteTeam,
+    editTeam,
+} from './team';
--- src/store/reducers/teams.js
+++ src/store/reducers/teams.js
@@ -1,9 +1,40 @@
+import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../../utils/utils';
+
 const initialState = {
     teams: []
 };
 
-const reducer = (state = initialState, action) => {
-    return state;
+const addTeam = (state, action) => {
+    return updateObject(state, {
+        teams: [...state.teams, action.team]
+    });
 };
 
-export default reducer;
+const deleteTeam = (state, action) => {
+    return updateObject(state, {
+        teams: state.teams.filter(team => team.id !== action.id)
+    });
+};
+
+const editTeam = (state, action) => {
+    const stateWithoutTeam = state.teams.filter(team => team.id !== action.id);
+    return updateObject(stateWithoutTeam, {
+        teams: [...stateWithoutTeam.teams, action.team]
+    });
+};
+
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case actionTypes.ADD_TEAM:
+            return addTeam(state, action);
+        case actionTypes.DELETE_TEAM:
+            return deleteTeam(state, action);
+        case actionTypes.EDIT_TEAM:
+            return editTeam(state, action);
+        default:
+            return state;
+    }
+};
+
+export default reducer; 
