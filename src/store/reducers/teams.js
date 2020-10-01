a21603ae5e09fcbc58c3930a91e3389fb18ac74d
Caue SJ <caue.silvaa@gmail.com>
2020-10-01 01:44:17 -0300

    Fix import name

--- src/store/reducers/teams.js
+++ src/store/reducers/teams.js
@@ -1,5 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
-import { updateObject } from '../../util/utility';
+import { updateObject } from '../../utils/utils';
 
 const initialState = {
     teams: []
